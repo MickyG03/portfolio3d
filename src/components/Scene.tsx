84cc1b9fbc057c316bb1448c01ab3b5e4eb7f7a0
MickyG <miteshgulecha@gmail.com>
2025-04-05 04:09:37 -0400

    textures and updates for allowing glb files

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,36 +1,23 @@
-import { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
 import { useThemeStore } from '../store/themeStore';
 import { useWeatherStore } from '../store/weatherStore';
-import { Mesh } from 'three';
-import { ThemeSwitch3D } from './ThemeSwitch3D';
 import { Rain } from './Rain';
 import { Wind } from './Wind';
 import { Snow } from './Snow';
+import { Model as Towers } from '../models/Towers';
+import { Model as Walls1 } from '../models/Walls1';
+import { Model as Stairs1 } from '../models/Stairs1';
+import { Model as Bridge } from '../models/Bridge';
 
 export const Scene = () => {
-  const meshRef = useRef<Mesh>(null);
   const { theme } = useThemeStore();
   const { currentWeather } = useWeatherStore();
 
-  useFrame((state, delta) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += delta * 0.2;
-      meshRef.current.rotation.y += delta * 0.3;
-    }
-  });
-
   return (
     <>
-      <mesh ref={meshRef}>
-        <octahedronGeometry args={[1, 0]} />
-        <meshStandardMaterial
-          color={theme.colors.accent}
-          roughness={0.5}
-          metalness={0.8}
-        />
-      </mesh>
-      <ThemeSwitch3D />
+      <Towers />
+      <Walls1 />
+      <Stairs1 />
+      <Bridge />
       {currentWeather === 'rainy' && <Rain />}
       {currentWeather === 'windy' && <Wind />}
       {currentWeather === 'snowy' && <Snow />}
