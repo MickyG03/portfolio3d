5b95f82801ee033be496249b1eb8551c188d05ed
MickyG <miteshgulecha@gmail.com>
2025-04-05 04:18:42 -0400

    level imported

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -8,6 +8,8 @@ import { Model as Walls1 } from '../models/Walls1';
 import { Model as Stairs1 } from '../models/Stairs1';
 import { Model as Bridge } from '../models/Bridge';
 import { Model as Stairs2 } from '../models/Stairs2';
+import { Model as Walls2 } from '../models/Walls2';
+import { Model as Castle } from '../models/Castle';
 
 export const Scene = () => {
   useThemeStore();
@@ -20,6 +22,8 @@ export const Scene = () => {
       <Stairs1 />
       <Bridge />
       <Stairs2 />
+      <Walls2 />
+      <Castle />
       {currentWeather === 'rainy' && <Rain />}
       {currentWeather === 'windy' && <Wind />}
       {currentWeather === 'snowy' && <Snow />}
